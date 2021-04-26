0b3059b4beffe69cb69ce41407631bf4d982b96b
Gerard Braad <me@gbraad.nl>
2021-04-26 15:15:10 +0000

    Force change for github pages!

--- js/github.js
+++ js/github.js
@@ -8,7 +8,7 @@ var PullRequestController = function() {
                 let htmlOut = ""
                         
                 data.forEach(pull => {
-                    console.log(pull)
+                    //console.log(pull)
                     let markedBody = ""; //marked(pull.body)
                     //htmlOut += `<li class="pull-request"><div class="pull-request-title"><a href="${pull.html_url}">${pull.title}</a></div><div class="pull-request-body">${markedBody}</div></li>`;
                     htmlOut += cardTemplate(pull.title, pull.html_url, markedBody, "Unknown")
