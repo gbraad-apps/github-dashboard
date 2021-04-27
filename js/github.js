3edbaf8190e54bd6fcd9765064e01d1111424c3e
Gerard Braad <me@gbraad.nl>
2021-04-27 11:19:39 +0000

    Synchronous statuses and filter for seen

--- js/github.js
+++ js/github.js
@@ -1,25 +1,52 @@
 var PullRequestController = function() {
 
-    var query = function(organisation, project) {
-        return fetch(`https://api.github.com/repos/${organisation}/${project}/pulls`)
-            .then(response => response.json())
-            .then(data => {
-                let results = document.querySelector('#cards')
-                let htmlOut = ""
-                        
-                data.forEach(pull => {
-                    console.log(pull)
-                    var labels = ""
-                    pull.labels.forEach(label => {
-                        labels += labelTemplate(label.name, label.color);
-                    })
-                    htmlOut += cardTemplate(pull.title,
-                        pull.html_url, labels, "Unknown")
-                })
+    var query = async function(organisation, project) {
+        const pullsRequest = await fetch(`https://api.github.com/repos/${organisation}/${project}/pulls`)
+        const pulls = await pullsRequest.json()
+        
+        let results = document.querySelector('#cards')
+        let htmlOut = ""
 
-                results.innerHTML = `<ul id="pull-requests">${htmlOut}</ul>`;
+        for (let index = 0; index < pulls.length; index++) {
+            var pull = pulls[index];
+
+            var labels = ""
+            pull.labels.forEach(label => {
+                labels += labelTemplate(label.name, label.color);
             })
-            .catch(error => console.log(error))
+
+            // `https://api.github.com/repos/${organisation}/${project}/statuses/${pull.head.sha}`
+            const statusesRequest = await fetch(pull.statuses_url)
+            const statuses = await statusesRequest.json()
+            var pending = 0, success = 0, failure = 0;
+            var seen = []
+
+            statuses.forEach(state => {
+                if(seen.includes(state.context)) {
+                    return; // skip
+                }
+                if(state.context.includes("centos")) {
+                    return; // skip
+                }
+                if(state.state == "pending") {
+                    pending++
+                }
+                if(state.state == "success") {
+                    success++;
+                }
+                if(state.state == "failure") {
+                    console.log(state)
+                    failure++
+                }
+                seen.push(state.context)
+            })
+            var status = statusTemplate(pending, success, failure)
+
+            htmlOut += cardTemplate(pull.title,
+                pull.html_url, labels, status)
+        }
+
+        results.innerHTML = `<ul id="pull-requests">${htmlOut}</ul>`;
     }
 
     // exposed functions
--- js/templates.js
+++ js/templates.js
@@ -1,13 +1,22 @@
-function cardTemplate(title, url, body, state) {
+function cardTemplate(title, url, body, status) {
     return `
         <div class="card">
             <h4 class="title"><a href="${url}">${title}</a></h4>
             <p>${body}</p>
-            <p class="state unknown">${state}</p>
+            ${status}
         </div>
     `
 }
 
 function labelTemplate(text, color) {
     return `<span class="badge" style="background-color: #${color}">${text}</span>`;
+}
+
+function statusTemplate(pending, success, failure) {
+    if (failure > 0)
+        return `<p class="state failure">Failure</p>`;
+    if (pending > 0)
+        return `<p class="state pending">Pending</p>`;
+    else
+        return `<p class="state success">Success</p>`;
 }
